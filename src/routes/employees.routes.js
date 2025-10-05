492ed886d3db8365e83afc6628978e40ab4d5eae
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2025-10-05 13:25:16 -0600

    feat: cambiar endpoint a español

--- src/routes/employees.routes.js
+++ src/routes/employees.routes.js
@@ -3,28 +3,28 @@ import { Router } from 'express'
 const router = Router()
 
 // Obtener todos los empleados
-router.get('/employees', (req, res) => {
+router.get('/empleados', (req, res) => {
   res.send('Obteniedo empleados')
 })
 
 // Obtener un empleado específico
-router.get('/employees/:id', (req, res) => {
+router.get('/empleados/:id', (req, res) => {
   const { id } = req.params
   res.send(`Obteniendo empleado con ID: ${id}`)
 })
 
 // Crear un nuevo empleado
-router.post('/employees', (req, res) => {
+router.post('/empleados', (req, res) => {
   res.send('Creando un nuevo empleado')
 })
 
 // Editar un empleado específico
-router.put('/employees/:id', (req, res) => {
+router.put('/empleados/:id', (req, res) => {
   res.send('Editando un empleado')
 })
 
 // Eliminar un empleado en específico
-router.delete('/employees/:id', (req, res) => {
+router.delete('/empleados/:id', (req, res) => {
   const { id } = req.params
   res.send(`Eliminando empleado con ID: ${id}`)
 })
