bc19edbbe118ca834ec3578ad27c8910a974a0e7
Douglas Barrera <53149834+Doisaac@users.noreply.github.com>
2025-10-05 12:46:48 -0600

    feat: agregar las rutas de empleado

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import express from "express"
 import { PORT } from "./config.js"
+import employeesRoutes from "./routes/employees.routes.js"
 
 const app = express()
+app.use(employeesRoutes)
 
 app.listen(PORT)
 console.log(`Server is running on ${PORT}`)
